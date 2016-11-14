eefd09ffa2222839d8d0a25eac72c7b7ea42f9c5
Julie Pierre <julie.pierre3@gmail.com>
2016-11-14 16:51:39 +0100

    added time

--- app/controllers/longestword_controller.rb
+++ app/controllers/longestword_controller.rb
@@ -5,6 +5,8 @@ class LongestwordController < ApplicationController
 
   def game
     @grid = generate_grid(9)
+    @now = Time.now
+    @start_time = @now.min * 60 + @now.sec
   end
 
   def generate_grid(grid_size)
@@ -12,11 +14,15 @@ class LongestwordController < ApplicationController
   end
 
   def score
+    @end = Time.now
+    @end_time = @end.min * 60 + @end.sec
     @attempt = params[:attempt]
     @grid = params[:grid].split("")
+    @start_time = params[:time].to_i
     @translation = get_translation(@attempt)
-    @score = compute_score(@attempt)
-    @result = score_and_message(@attempt, @translation, @grid)
+    @time = @end_time - @start_time
+    @score = compute_score(@attempt, @time)
+    @result = score_and_message(@attempt, @translation, @grid, @time)
     if session[:games] == nil
       session[:games] = 1
     else
@@ -46,8 +52,8 @@ class LongestwordController < ApplicationController
       end
   end
 
-  def compute_score(attempt)
-    attempt.size
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
   end
 
   def included?(guess, grid)
@@ -55,16 +61,16 @@ class LongestwordController < ApplicationController
     guess.all? { |letter| guess.count(letter) <= grid.count(letter) }
   end
 
-  def score_and_message(attempt, translation, grid)
-      if included?(attempt.upcase, grid)
-        if translation
-          score = compute_score(attempt)
-          [score, "well done"]
-        else
-          [0, "not an english word"]
-        end
+  def score_and_message(attempt, translation, grid, time)
+    if included?(attempt.upcase, grid)
+      if translation
+        score = compute_score(attempt, time)
+        [score, "well done"]
       else
-        [0, "not in the grid"]
+        [0, "not an english word"]
       end
+    else
+      [0, "not in the grid"]
+    end
   end
 end
